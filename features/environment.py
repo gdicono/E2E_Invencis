0a0e4297c196c41dd81c486fd4324811ec03eec0
Gonzalo Di Cono <gonzalo.dicono@correo.ucu.edu.uy>
2026-02-24 19:59:53 -0300

    Agregue selectores del front  en iniciar sesion

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright #Herramiento de automatiizacion de navegadores
 
-def before_all(context): # Encender
+def before_all(context): # Encender todo
     context.playwright = sync_playwright().start() # Inicia el Playwright
     context.browser = context.playwright.chromium.launch( # Abre el Chrome
         headless=False, # Vemos el navegador abrirse. Si fuera True la accion pasaria en segundo plano
@@ -8,6 +8,9 @@ def before_all(context): # Encender
     )
     context.context = context.browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
     context.page = context.context.new_page() # Crea un contexto de navegador
+
+    context.page = context.context.new_page()
+    context.page.goto("http://localhost:5173/login")
 
 def after_all(context):
     context.browser.close() # Cerramos navegador
--- features/steps/RF-06_steps.py
+++ features/steps/RF-06_steps.py
@@ -6,16 +6,16 @@ from pages.base_page import BasePage
 def step_login(context):
     context.base_page = BasePage(context.page)
 
-    context.page.goto("http://localhost:5173")
+    context.page.goto("http://localhost:5173/login")
 
-    context.base_page.fill()
-    context.base_page.fill()
-    context.base_page.click()
-
+    context.base_page.fill("//input[@name='email']", "usuario@demo.com") # Ingresamos usuario de prueba
+    context.base_page.fill("//input[@name='password']", "123456") # Ingresamos contraseña del usuario de prueba
+    context.base_page.click("//button[text()='Iniciar Sesión']") # Hacemos click en el boton de iniciar sesion
+    context.page.wait_for_timeout(3000) # Pausa para que no se cargue todo tan tapido
 
 @when("selecciona el módulo de 'mentoría'")
 def step_ir_mentoria(context):
-    context.base_page.click()
+    context.base_page.click("//span[text()='Sesiones']") # Click en boton sesiones
 
 
 @when("selecciona una sesión activa")
